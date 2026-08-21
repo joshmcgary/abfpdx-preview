68b8772ac1b55fb4937b3e0bcf60198b7f4c1aae
Josh McGary <joshsair@Mac.lan>
2026-08-21 08:21:47 -0700

    Disable PWA cache during local development

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,21 @@ import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import './styles.css'
 
-const updateSW = registerSW({
-  immediate: true,
-  onNeedRefresh() {
-    updateSW(true)
-  },
-  onRegisteredSW(_url, registration) {
-    registration?.update()
-  },
-})
+if (import.meta.env.PROD) {
+  const updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+      updateSW(true)
+    },
+    onRegisteredSW(_url, registration) {
+      registration?.update()
+    },
+  })
+} else if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister())
+  })
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
