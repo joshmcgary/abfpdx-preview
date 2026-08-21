42a4ddf1d8572c69a3e35613c3dbdff40b7dbef6
Josh McGary <joshsair@Mac.lan>
2026-08-21 07:56:31 -0700

    Refresh stale PWA versions automatically

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,15 @@ import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import './styles.css'
 
-registerSW({ immediate: true })
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    updateSW(true)
+  },
+  onRegisteredSW(_url, registration) {
+    registration?.update()
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
